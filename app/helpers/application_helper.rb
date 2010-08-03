c73f3cbccc9cc9eed6dccd8e232399dc89ddbad5
maxschulze <max.schulze@gmail.com>
2010-08-03 02:17:20 +0200

    removed old helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
   
-  def page_title(title = "")
-    if title.present?
-      @title = t(title)
-    else
-      return "#{@title || action_name}"
-    end
-  end
-  
 end
