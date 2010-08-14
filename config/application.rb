e628d4aa66ee1d51830c5851cdc8c2377e4acb01
maxschulze <max.schulze@gmail.com>
2010-08-14 18:36:05 +0200

    change to i18n

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module Ems
     # -- all .rb files in that directory are automatically loaded.
 
     # Add additional load paths for your own custom dirs
-    config.autoload_paths += %W( #{config.root}/app/observers #{config.root}/lib #{config.root}/app/viewers #{config.root}/lib #{config.root}/app/middleware )
+    config.autoload_paths += %W( #{config.root}/app/observers #{config.root}/lib #{config.root}/app/viewers #{config.root}/app/middleware )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
@@ -25,7 +25,6 @@ module Ems
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
-    I18n.backend = I18n::Backend::Database.new
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
     config.generators do |g|
