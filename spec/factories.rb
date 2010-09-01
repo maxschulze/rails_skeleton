cd8ca1c87d5e3b00608beb9a68926f32ef68f227
Jan Riethmayer <jan@riethmayer.de>
2010-09-02 00:12:40 +0200

    added a user_spec to have a starting point, which tests the factory_girl factories.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,20 @@
-# Factory.define :person do |f|
-#   f.first_name            'Max'    
-#   f.last_name             'Schulze'
-#   f.salutation            'Dr. Prof.'
-#   f.title                 'CTO'    
-#   f.state                 'active' 
-#   f.important             false
-#   f.gender                'male'   
-#   f.birthdate             Date.new(1989, 9, 16)
-#   f.relationship_status   'single'
-#   f.notes                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut'
-#   f.visible_to            1
-#   f.association           :company, :factory => :company
-# end
+#----------------------------------------------------------------------#
+# sequences first
+#----------------------------------------------------------------------#
+Factory.sequence :email do |n|
+  "person#{n}@example.com"
+end
+
+#----------------------------------------------------------------------#
+# factories, which do not rely on other factories
+#----------------------------------------------------------------------#
+Factory.define :user do |f|
+  f.email Factory.next :email
+  f.password "secret"
+  f.password_confirmation "secret"
+end
+
+#----------------------------------------------------------------------#
+# factories with associations
+#----------------------------------------------------------------------#
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe User do
+  describe "Factory" do
+    it "should be valid by default" do
+      Factory(:user).should be_valid
+    end
+  end
+end
