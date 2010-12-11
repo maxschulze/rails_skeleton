c8efd85e3097aa5d1c681f0bc4ebb1ed85c7e17b
Max Schulze <max@maxschulze.com>
2010-12-11 16:03:23 +0100

    removed spork

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,44 +2,39 @@
 ENV["RAILS_ENV"] ||= "test"
 
 require 'rubygems'
-# require 'spork'
 
-Spork.prefork do
-  require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 
-  # rspec
-  require 'rspec/rails'
+# rspec
+require 'rspec/rails'
 
-  # cucumber
-  require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
-  require 'cucumber/rails/world'
-  require 'cucumber/rails/rspec'
-  require 'cucumber/rails/active_record'
-  require 'cucumber/web/tableish'
+# cucumber
+require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
+require 'cucumber/rails/world'
+require 'cucumber/rails/rspec'
+require 'cucumber/rails/active_record'
+require 'cucumber/web/tableish'
 
-  # capybara
-  require 'capybara'
-  require 'capybara/rails'
-  require 'capybara/session'
-  require 'capybara/cucumber'
-  # require 'cucumber/rails/capybara_javascript_emulation'
+# capybara
+require 'capybara'
+require 'capybara/rails'
+require 'capybara/session'
+require 'capybara/cucumber'
+# require 'cucumber/rails/capybara_javascript_emulation'
 
-  Capybara.default_selector = :css
+Capybara.default_selector = :css
 
-  # email spec
-  require 'email_spec/cucumber'
+# email spec
+require 'email_spec/cucumber'
 
-  # factory girl
-  require 'factory_girl'
-  require 'factory_girl/step_definitions'
-end
+# factory girl
+require 'factory_girl'
+require 'factory_girl/step_definitions'
 
-Spork.each_run do
-  ActionController::Base.allow_rescue = false
-  Cucumber::Rails::World.use_transactional_fixtures = true
+ActionController::Base.allow_rescue = false
+Cucumber::Rails::World.use_transactional_fixtures = true
 
-  require 'database_cleaner'
+require 'database_cleaner'
 
-  DatabaseCleaner.strategy = :truncation, { :except => %w[ translations ] }
-  DatabaseCleaner.clean
-end
+DatabaseCleaner.strategy = :truncation, { :except => %w[ translations ] }
+DatabaseCleaner.clean
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,33 +1,24 @@
 require 'rubygems'
-# require 'spork'
 
-Spork.prefork do
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
 
-  require 'rspec/rails'
-  require "paperclip/matchers"
+require 'rspec/rails'
+require "paperclip/matchers"
 
-end
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-Spork.each_run do
+RSpec.configure do |config|
+  config.mock_with :mocha
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  config.include Paperclip::Shoulda::Matchers
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
 
-  RSpec.configure do |config|
-    config.mock_with :mocha
-
-    config.include Paperclip::Shoulda::Matchers
-    config.include(EmailSpec::Helpers)
-    config.include(EmailSpec::Matchers)
-
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = true
-  end
-
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
 end
