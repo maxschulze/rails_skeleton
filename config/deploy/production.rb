f7573a5b4ee90bc0d616ecc33b06ab7f871a0bb0
Max Schulze <max@maxschulze.com>
2010-12-11 15:55:03 +0100

    added database tasks to capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,22 +11,22 @@ set :rails_env,   "production"
 set :deploy_to,   "/home/applications/#{application}"
 set :deploy_via,  :remote_cache
 
-role :app,        "app3.urbanvention.com"
-role :web,        "app3.urbanvention.com"
-role :db,         "app3.urbanvention.com", :primary => true
+role :app,        "188.165.119.69"
+role :web,        "188.165.119.69"
+role :db,         "188.165.119.69", :primary => true
 
 set :use_sudo, false
 ssh_options[:paranoid] = false
 
 namespace :deploy do
-  
+
   desc "Restart Application"
   task :restart, :roles => :app do
     ['system'].each do |link|
       puts "create Symlink for #{link}"
       run "ln -nfs  #{deploy_to}/shared/public/#{link} #{deploy_to}/current/public/#{link}"
     end
-    
+
     run "touch #{deploy_to}/current/tmp/restart.txt"
   end
 
@@ -49,12 +49,12 @@ namespace :deploy do
   task :search_index, :roles => :app do
     run "cd #{current_path} && rake ts:in RAILS_ENV=#{rails_env}"
   end
-  
+
   desc "Symlink Sphinx"
   task :symlink_sphinx_indexes, :roles => [:app] do
     run "ln -nfs #{deploy_to}/shared/db/sphinx #{deploy_to}/current/db/sphinx"
   end
-    
+
 end
 
 namespace :cache do
